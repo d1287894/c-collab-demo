71529801814b64f3cb9661b618ad3340c568dfdd
D1260499 <D1260499@o365.fcu.edu.tw>
2025-12-05 10:26:44 +0800

    feat: sub addition

--- main.c
+++ main.c
@@ -3,7 +3,9 @@
 int main() {
     printf("=== C Language Calculator ===\n");
     
-    // Operations placeholders
+    int a = 10;
+    int b = 20;
+    printf("Subtraction: %d - %d = %d\n", a, b, a-b);
     
     printf("System Check OK\n");
     return 0;

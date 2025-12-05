a9c958f19dbb7786b5670a7faa8b944713c409b5
d1282909 <d1282909@o365.fcu.edu.tw>
2025-12-05 10:12:20 +0800

    feat: add addition

--- main.c
+++ main.c
@@ -4,7 +4,10 @@ int main() {
     printf("=== C Language Calculator ===\n");
     
     // Operations placeholders
-    
+    int a = 10;
+    int b = 20;
+    printf("Addition: %d + %d = %d\n", a, b, a + b);
+
     printf("System Check OK\n");
     return 0;
 }
